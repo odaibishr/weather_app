19aed949ed398e406cc757cd505a7fde8d543471
Odai Bishr <odaibishr@gmail.com>
2025-08-22 00:23:22 +0300

    weather app & change form provider state managment to bloc

---
+++ lib/cubits/weather_cubit/weather_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_state.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+class WeatherCubit extends Cubit<WeatherState> {
+  WeatherCubit({required this.weatherService}) : super(WeatherInitial());
+
+  WeatherService weatherService;
+  WeatherModel? weatherModel;
+  String? cityName;
+
+  void getWeather({required String cityName}) async {
+    emit(WeatherLoading());
+    try {
+      weatherModel = await weatherService.getWeather(cityName: cityName);
+      emit(WeatherSuccess());
+    } catch (e) {
+      emit(WeatherFailure());
+    }
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_cubit.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_state.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/pages/search_page.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  WeatherModel? weatherData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return SearchPage();
+                }));
+              },
+              icon: const Icon(Icons.search),
+            ),
+          ],
+          title: const Text('Weather App'),
+        ),
+        body:
+            BlocBuilder<WeatherCubit, WeatherState>(builder: (context, state) {
+          if (state is WeatherLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is WeatherSuccess) {
+            weatherData = BlocProvider.of<WeatherCubit>(context).weatherModel!;
+            return Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                colors: [
+                  BlocProvider.of<WeatherCubit>(context)
+                      .weatherModel!
+                      .getThemeColor(),
+                  BlocProvider.of<WeatherCubit>(context)
+                      .weatherModel!
+                      .getThemeColor()[300]!,
+                  BlocProvider.of<WeatherCubit>(context)
+                      .weatherModel!
+                      .getThemeColor()[100]!,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Spacer(
+                    flex: 3,
+                  ),
+                  Text(
+                    BlocProvider.of<WeatherCubit>(context).cityName!,
+                    style: const TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'updated at : ${BlocProvider.of<WeatherCubit>(context).weatherModel!.date.hour.toString()}:${BlocProvider.of<WeatherCubit>(context).weatherModel!.date.minute.toString()}',
+                    style: const TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Image.asset(BlocProvider.of<WeatherCubit>(context)
+                          .weatherModel!
+                          .getImage()),
+                      Text(
+                        BlocProvider.of<WeatherCubit>(context)
+                            .weatherModel!
+                            .temp
+                            .toInt()
+                            .toString(),
+                        style: const TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text('maxTemp :${weatherData!.maxTemp.toInt()}'),
+                          Text('minTemp : ${weatherData!.minTemp.toInt()}'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  Text(
+                    weatherData!.weatherStateName,
+                    style: const TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 5,
+                  ),
+                ],
+              ),
+            );
+          } else if (state is WeatherFailure) {
+            return const Center(
+              child: Text('Something went wrong, try again later'),
+            );
+          } else {
+            return const Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'there is no weather 😔 start',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  Text(
+                    'searching now 🔍',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+        }));
+  }
+}
